178ac04e73e3d765cf321f4aef020547cf1bd76e
SnJon <napstertm@gmail.com>
2022-11-06 18:00:37 +0900

    add createComment & createReportComment + tests

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -6,7 +6,7 @@ class WallServiceTest {
 
     @Test
     fun add() {
-        val testPost = Post(0, 22102022, 11, 29102022, "Test post", 5)
+        val testPost = Post(null, 22102022, 11, 29102022, "Test post", 5)
         val (result) = WallService.add(testPost)
         assertEquals(1, result)
     }
@@ -18,7 +18,7 @@ class WallServiceTest {
 
     @Test
     fun updateExisting() {
-        val testPost = Post(0, 22102022, 11, 29102022, "Test post", 5)
+        val testPost = Post(null, 22102022, 11, 29102022, "Test post", 5)
         WallService.add(testPost)
         val result = WallService.update(testPost)
         assertEquals(true, result)
@@ -31,8 +31,60 @@ class WallServiceTest {
 
     @Test
     fun updateNonexistent() {
-        val testPost = Post(0, 22102022, 11, 29102022, "Test post", 5)
+        val testPost = Post(null, 22102022, 11, 29102022, "Test post", 5)
         val result = WallService.update(testPost)
         assertEquals(false, result)
+    }
+
+    @Test
+    fun createComment() {
+        val testPost = Post(null, 11, 111, 30102022, "Test post", 3)
+        WallService.add(testPost)
+        val id = 1
+        val comment = Comment(id, 111, 6112022, "New comment", 11)
+        val result = WallService.createComment(id, Comment(date = 6112022, text = "New comment"))
+        assertEquals(comment, result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun shouldThrowPost() {
+        val testPost = Post(null, 11, 111, 30102022, "Test post", 3)
+        WallService.add(testPost)
+        val postId = 2
+        WallService.createComment(
+            postId,
+            Comment(date = 6112022, text = "New comment")
+        ) ?: throw PostNotFoundException("No post with id: $postId")
+    }
+
+    @Test
+    fun createReportComment() {
+        val testPost = Post(null, 11, 111, 30102022, "Test post", 3)
+        WallService.add(testPost)
+        val postId = 1
+        WallService.createComment(postId, Comment(postId, 111, 6112022, "New comment", 11))
+        val commentId = 1
+        val reportComment = ReportComment(commentId, 111, 1, Reasons.ABUSE)
+        val result = WallService.createReportComment(commentId, Reasons.ABUSE)
+        assertEquals(reportComment, result)
+    }
+
+    @Before
+    fun clearBeforeShouldThrowComment() {
+        WallService.clear()
+    }
+
+    @Test(expected = CommentNotFoundException::class)
+    fun shouldThrowComment() {
+        val testPost = Post(null, 11, 111, 30102022, "Test post", 3)
+        WallService.add(testPost)
+        val postId = 1
+        WallService.createComment(
+            postId,
+            Comment(date = 6112022, text = "New comment")
+        )
+        val commentId = 2
+        WallService.createReportComment(commentId, Reasons.DRUG_PROPAGANDA)
+            ?: throw CommentNotFoundException("No comment with id: $commentId")
     }
 }
